04e63d7d8acf9849922ab3b97297668c7b0df967
정현진 <jhj9328@hanmail.net>
2022-12-16 11:39:52 +0900

    saving username by using localStorage

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
+  localStorage.setItem("username", username);
   greeting.innerText = `hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
