805b24879efd78c745dc0f1a401f06a2af33ca3c
정현진 <jhj9328@hanmail.net>
2022-12-16 18:31:10 +0900

    padStart edition

--- js/clock.js
+++ js/clock.js
@@ -3,10 +3,13 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
-      const date = new Date();
-      clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+  const date = new Date();
+  const hour = String(date.getHours()).padStart(2,"0");
+  const minute = String(date.getMinutes()).padStart(2,"0");
+  const second = String(date.getSeconds()).padStart(2,"0");
+  clock.innerText = `${hour}:${minute}:${second}`;
 }
 
-//getClock함수가 나온 이유는 setInterval로 1초 기다리지 않고 바로 시간 보여주려고 
+//getClock함수가 나온 이유는 setInterval로 1초 기다리지 않고 바로 시간 보여주려고
 getClock();
-setInterval(getClock,1000);
+setInterval(getClock, 1000);
