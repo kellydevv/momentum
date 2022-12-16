ea6d5f6ce9412632fd727895dde2a81e5702e90e
정현진 <jhj9328@hanmail.net>
2022-12-16 14:42:21 +0900

    loading username from localStorage

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
-  greeting.innerText = `hello ${username}`;
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings();
+}
+
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${savedUsername} `;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+if (savedUsername === null) {
+  //show the form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //show greeting h1
+  paintGreetings();
+}
